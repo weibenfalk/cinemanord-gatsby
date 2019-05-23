3fc79035fa4271bccb305b683c04de589aef7314
Thomas Weibenfalk <thomas.weibenfalk@gmail.com>
2019-05-23 14:28:36 +0200

    3

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,21 @@ import HomeLayout from '../components/HomeLayout';
 import HomeMenu from '../components/HomeMenu';
 import Quote from '../components/Quote';
 
-import MailWidget from '../components/MailWidget';
-
 import { GlobalStyle } from '../components/styles/StyledLayout';
 
 const IndexPage = () => (
   <>
+    <form
+      name="contact"
+      method="post"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      {/* You still need to add the hidden input with the form name to your JSX form */}
+      <input type="hidden" name="form-name" value="contact" />
+      <input type="text" />
+      <input type="submit" />
+    </form>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <HomeHero />
     <HomeLayout>
